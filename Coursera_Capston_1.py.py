91f6751db4073929f4a9bcb6f3cd1f615987dccc
unknown <yangdonghun3@gmail.com>
2020-03-24 13:26:20 +0900

    add Coursera_capston_1

---
+++ Coursera_Capston_1.py.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for Coursera Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
